3522138d125c515114e19e749fc564aa58b98404
Akshadkurundwade07 <akshadkurundwade181818@gmail.com>
2025-07-25 21:38:11 +0530

    some progress in frontend

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 import pandas as pd 
 app = Flask(__name__)
-car = pd.read_csv('cleaned_cars.csv')
+car = pd.read_csv('cleaned_car.csv')
 
 
 @app.route('/')
