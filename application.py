7d3c4a9c6b629446e83b67a660cdcc58c9a5f2b9
Akshadkurundwade07 <akshadkurundwade181818@gmail.com>
2025-07-26 22:54:25 +0530

    error

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import pandas as pd 
+
 app = Flask(__name__)
 car = pd.read_csv('cleaned_car.csv')
-
 
 @app.route('/')
 def index():
